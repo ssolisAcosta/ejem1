013e08251236aef4d18e34708324886cd43d1f79
SSolis <SSolis@SSISTEMAS>
2017-10-17 10:45:37 -0500

    V1. segunda versión

--- src/ejemplo1/Ejemplo1.java
+++ src/ejemplo1/Ejemplo1.java
@@ -5,6 +5,8 @@
  */
 package ejemplo1;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Labs-DECC
@@ -17,6 +19,7 @@ public class Ejemplo1 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("hola mundo");
+        JOptionPane.showMessageDialog(null,"Hola");
     }
     
 }
